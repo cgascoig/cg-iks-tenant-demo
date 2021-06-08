cc54419ce1e921afeedb0bf85904e2f2528547c2
Chris Gascoigne <cgascoig@cisco.com>
2021-06-08 22:26:25 +1000

    update

--- providers.tf
+++ providers.tf
@@ -9,16 +9,16 @@ provider "helm" {
   }
 }
 
-terraform {
-  required_providers {
-    intersight = {
-      source = "CiscoDevNet/intersight"
-      version = "1.0.9"
-    }
-  }
-}
-provider "intersight" {
-  apikey    = var.intersight_api_key
-  secretkey = var.intersight_secretkey
-  endpoint  = var.intersight_endpoint
-}
+# terraform {
+#   required_providers {
+#     intersight = {
+#       source = "CiscoDevNet/intersight"
+#       version = "1.0.9"
+#     }
+#   }
+# }
+# provider "intersight" {
+#   apikey    = var.intersight_api_key
+#   secretkey = var.intersight_secretkey
+#   endpoint  = var.intersight_endpoint
+# }
